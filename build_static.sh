117151dd6fca5671667049114980ae844b226f64
Ariel Weisberg <aweisberg@voltdb.com>
2010-10-12 16:34:01 +0000

    Modify the script to produce a tgz with the binary and includes

--- build_static.sh
+++ build_static.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm libvoltcpp.a
+rm -fr *.a *.gz tmp package
 cd library_release_static
 make clean
 make
@@ -23,12 +23,19 @@ ar x $HOME/lib/libboost_thread.a
 cd .. && mkdir boost_system && cd boost_system
 ar x $HOME/lib/libboost_system.a
 cd .. && mkdir boost_filesystem && cd boost_filesystem
-cd .. && mkdir boost_thread && cd boost_thread
-ar x $HOME/lib/libboost_thread.a
-cd .. && mkdir boost_filesystem && cd boost_filesystem
 ar x $HOME/lib/libboost_filesystem.a
 cd .. && mkdir crypto && cd crypto
 ar x $HOME/lib/libcrypto.a
 cd ../..
 ar rcs libvoltcpp.a tmp/volt/* tmp/event/* tmp/boost_system/* tmp/boost_thread/* tmp/boost_filesystem/* tmp/crypto/* tmp/boost_thread/*
+
+mkdir package
+mkdir package/include
+cd include
+cp -r ByteBuffer.hpp Client.h ClientConfig.h Column.hpp ConnectionPool.h Decimal.hpp Exception.hpp InvocationResponse.hpp Parameter.hpp ParameterSet.hpp Procedure.hpp ProcedureCallback.hpp Row.hpp RowBuilder.h StatusListener.h Table.h TableIterator.h WireType.h ttmath/*.h boost ../package/include
+cd ..
+cp libvoltcpp.a package
+tar cvfz volt_cpp.tgz package/*
+strip package/libvoltcpp.a
+tar cvfz volt_cpp_stripped.tgz package/*
 rm -fr tmp
--- clean.sh
+++ clean.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rm -fr package tmp *.a *.tgz
 cd AsyncHelloWorld && make clean
 cd ../HelloWorld && make clean
 cd ../library_release && make clean
