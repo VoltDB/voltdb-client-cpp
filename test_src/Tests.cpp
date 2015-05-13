9c7dd15401f7200fe6a52bca3f3cfd16ef7bf17c
Ruth Morgenstein <rmorgenstein@voltdb.com>
2015-05-13 11:55:04 -0400

    Make cpptest main return 0 for SUCCESS.

--- test_src/Tests.cpp
+++ test_src/Tests.cpp
@@ -28,6 +28,6 @@ int main (int argc, char **argv) {
     CppUnit::TextUi::TestRunner runner;
     CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
     runner.addTest( registry.makeTest() );
-    return runner.run( "" );
+    return !runner.run( "" );
 }
 
